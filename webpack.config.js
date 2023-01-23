79d85c777489f7890f6bf9fced2b6487ce615474
henry <henry@hotmail.com.tw>
2023-01-23 17:48:54 +0800

    [Add] init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/i, 
+        use: ['style-loader', 'css-loader'] 
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
+};
